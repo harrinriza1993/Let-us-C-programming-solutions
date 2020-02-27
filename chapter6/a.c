925bd431c54f43ee4965ff93e9008b31371b3b4a
harrinriza1993 <riza.peridot@gmail.com>
2020-02-27 19:36:19 +0100

    Changed the file name

--- chapter6/a.c
+++ chapter6/a.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+
 void main()
 {
 	int num,i;
+
 	printf("enter a number");
 	scanf("%d",&num);
+
 	for ( i=2;i<num;i++)
 	{
 		if (num%i==0)
@@ -11,14 +14,13 @@ void main()
 			printf("Not a prime number\n");
 			break;
 		}
-	
-		}
-		if (i==num)
 
-		{
-			printf("prime number");
-			
-		}
-	
+	}
+
+	if (i==num)
+	{
+		printf("prime number");
+		
+	}
 }
 
