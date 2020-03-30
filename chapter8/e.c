961d052c40dc31c1c26c3f3c3f061b3923534ced
harrinriza1993 <riza.peridot@gmail.com>
2020-03-30 12:14:53 +0200

    Solution for chapter8

--- chapter8/e.c
+++ chapter8/e.c
@@ -5,7 +5,7 @@ Apporach:
 	1)To get the factors of a given number,the number should be divided 
 	  by 2 to less than the given number.
 	2)This can be done by a modulo operator.
-	3)If we get zero,then the divided numbers are considered as Prime factors.
+	3)In the factors the prime numbers are considered as prime factors.
 */
 
 #include<stdio.h>
@@ -23,15 +23,25 @@ void main()
 }
 void factor(int number)
 {
-	int index;
+	int i,j;
 
-	for ( index = 2;index < number; index++)
+	for ( i = 2;i <= number; i++)
 	{
-		/*This condition is checked and the index numbers which satisfy
-		  this condition are prime factors.*/
-		if (number%index == 0)
-			printf ("The factors of  %d is %d\n",number,index);
+		if (number%i == 0)
+		{
+			for (j=2;j <= i;j++)
+			{
+				if (i%j == 0)
+					break;
+			}
+			if (i == j)
+			{
+				printf("The prime factors of a number are %d\n",i);	
+			}
+		}
 	}
 }
 
 	
+
+
